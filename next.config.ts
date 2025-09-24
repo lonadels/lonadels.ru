0e6de8a0bb96ef1a16eaae593583bb79ab1dafc6
Шинкарук Владислав <vladislav.shinkaruk@irb.rest>
2025-09-24 07:32:47 +0300

    feat(serwist): add serwist

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import withSerwistInit from '@serwist/next';
+
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
 
 const nextConfig: NextConfig = {
     output: "standalone",
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
