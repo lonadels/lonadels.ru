1c3a7bd85c1a329946e32f7f0b5f29cc65bff1a4
Шинкарук Владислав <vladislav.shinkaruk@irb.rest>
2025-09-24 09:34:37 +0300

    refactor(api): validate ip, clean code

--- src/app/api/createProxyKey/route.ts
+++ src/app/api/createProxyKey/route.ts
@@ -2,30 +2,25 @@ import {NextResponse} from 'next/server';
 import db from '@/lib/db';
 import vpn from '@/lib/vpn';
 import {headers} from 'next/headers';
+import {isValidIP} from '@/lib/utils';
 
 export async function POST() {
   const headersList = await headers();
   const ip = headersList.get('x-forwarded-for');
 
-  if (!ip)
-    return NextResponse.json({error: 'Can\'t get IP'}, {status: 400});
+  if (!isValidIP(ip))
+    return NextResponse.json({error: 'Invalid or missing IP address'}, {status: 400});
 
-  if( ip === process.env.HOST_IP! )
+  if (ip === process.env.HOST_IP!)
     return NextResponse.json({error: 'Please, disable VPN first'}, {status: 400});
 
-  const {accessUrl} = await vpn.createAccessKey();
-
   const device = await db.devices.upsert({
-    where: {
-      ip,
-    },
-    create: {
-      ip,
-    },
+    where: {ip},
+    create: {ip},
     update: {},
   });
 
-  const key = await db.proxyKey.findFirst({
+  let proxyKey = await db.proxyKey.findFirst({
     where: {
       devices: {
         none: {
@@ -35,27 +30,27 @@ export async function POST() {
     },
   });
 
-  const proxyKey = key && await db.proxyKey.update({
-    where: {
-      id: key.id,
-    },
-    data: {
-      devices: {
-        connect: {
-          id: device.id,
+  if (proxyKey) {
+    proxyKey = await db.proxyKey.update({
+      where: {id: proxyKey.id},
+      data: {
+        devices: {
+          connect: {id: device.id},
         },
       },
-    },
-  }) || await db.proxyKey.create({
-    data: {
-      accessUrl,
-      devices: {
-        connect: {
-          id: device.id,
+    });
+  } else {
+    const {accessUrl} = await vpn.createAccessKey();
+
+    proxyKey = await db.proxyKey.create({
+      data: {
+        accessUrl,
+        devices: {
+          connect: {id: device.id},
         },
       },
-    },
-  });
+    });
+  }
 
   return NextResponse.json(proxyKey, {status: 200});
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function isValidIP(ip: string | null): ip is string {
+  if (!ip) return false;
+
+  const ipRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
+  if (!ipRegex.test(ip)) return false;
+
+  return ip.split('.').every(octet => {
+    const num = parseInt(octet, 10);
+    return num >= 0 && num <= 255;
+  });
+}
