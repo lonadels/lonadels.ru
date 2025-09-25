067aa0e8004a0951eea8e8e01ce20aceb515ec55
Шинкарук Владислав <vladislav.shinkaruk@irb.rest>
2025-09-25 19:42:59 +0300

    fix(api): validate client IP and associate devices with proxy keys
    Added client IP validation using `extractClientIp` and `isValidIP`. Implemented logic to upsert device records and link them to proxy keys if the IP address differs from `HOST_IP`.

--- src/lib/server/keys.ts
+++ src/lib/server/keys.ts
@@ -1,11 +1,27 @@
-import { findProxyKeyByUuid } from '@/app/api/keys/repository';
+import {connectDeviceToProxyKey, findProxyKeyByUuid, upsertDevice} from '@/app/api/keys/repository';
 import type { GetProxyKeyResponse } from '@/lib/types';
+import {extractClientIp, isValidIP} from '@/lib/utils';
+import {headers} from 'next/headers';
+import HttpError from '@/lib/httpError';
 
 export async function getProxyKeyFromDB(uuid: string): Promise<GetProxyKeyResponse> {
+  const headersList = await headers();
+  const ip = extractClientIp(headersList);
+
+  if (!isValidIP(ip))
+    throw new HttpError(400, 'Invalid or missing IP address');
+
   const row = await findProxyKeyByUuid(uuid);
+
   if (!row) {
     throw new Error('Invalid proxy key uuid');
   }
+
+  if (ip !== process.env.HOST_IP) {
+    const device = await upsertDevice(ip);
+    await connectDeviceToProxyKey(row.id, device.id);
+  }
+
   return {
     accessUrl: row.accessUrl,
     createdAt: row.createdAt as unknown as Date,
